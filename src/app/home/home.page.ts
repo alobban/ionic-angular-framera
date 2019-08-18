e1cfbf1f77068a187beb8b2cebdfdf82f8936072
Andrew Lobban <andrewglobban@gmail.com>
2019-08-18 16:39:39 -0500

    Add Team card with toggle

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,20 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 import * as moment from 'moment/moment';
-import {ActionSheetController} from '@ionic/angular';
+import { ActionSheetController } from '@ionic/angular';
+import { HttpServiceService } from '../http-service.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
   pigCount = 1345;
   groupAge = 7;
   groupId = 20190214;
   startDate = '2018-09-01';
+  team: any[] = [];
+  displayMember;
+  displayAll = false;
 
-  constructor(public actionSheetCtrl: ActionSheetController) {}
+  constructor(
+      public actionSheetCtrl: ActionSheetController,
+      private httpService: HttpServiceService
+  ) {}
 
   async presentActionSheet() {
     const actionSheet = await this.actionSheetCtrl.create({
@@ -34,6 +41,28 @@ export class HomePage {
       ]
     });
     await actionSheet.present();
+  }
+
+  toggleMembers() {
+    this.displayAll = !this.displayAll;
+  }
+
+  ngOnInit(): void {
+    this.httpService.getTeamMember()
+      .subscribe(data => {
+        console.log('team', data.results);
+        this.team = data.results;
+        this.generateCheckInTimes();
+        console.log('member after checkin generation', this.team);
+        this.displayMember = this.team[0];
+      });
+  }
+
+  generateCheckInTimes() {
+    this.team = this.team.map(member => {
+      member.login.last = new Date(moment().subtract(Math.floor(Math.random() * 24), 'hour').format());
+      return member;
+    });
   }
 
 }
