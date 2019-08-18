bdf671882a27c526dcd36ff91bd7bdfa5a0224d5
Andrew Lobban <andrewglobban@gmail.com>
2019-08-18 18:14:21 -0500

    Install @angular/flex-layout

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,14 +3,17 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MomentModule } from 'angular2-moment';
 
 import { HomePage } from './home.page';
-import {MomentModule} from 'angular2-moment';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    FlexLayoutModule,
     IonicModule,
     RouterModule.forChild([
       {
