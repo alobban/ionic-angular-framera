fd3e2ccb6bf82b4ef085b965039e4640f7d1c09a
Andrew Lobban <andrewglobban@gmail.com>
2019-08-19 20:18:07 -0500

    Animate toggle button and card

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,121 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {animate, animateChild, query, state, style, transition, trigger} from '@angular/animations';
 
 import * as moment from 'moment/moment';
 import { ActionSheetController } from '@ionic/angular';
+
 import { HttpServiceService } from '../http-service.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  animations: [
+      trigger('initState', [
+        state('collapse', style({
+          transform: 'rotate(0)'
+        })),
+        state('expand', style({
+          transform: 'rotate(-180deg)'
+        })),
+        transition('collapse => expand', [
+          animate(400, style({
+            transform: 'rotate(-180deg)'
+          }))
+        ]),
+        transition('expand => collapse', [
+          animate(400, style({
+            transform: 'rotate(0)'
+          }))
+        ])
+      ]),
+      trigger('returnState', [
+        state('collapse', style({
+          transform: 'rotate(180deg)'
+        })),
+        state('expand', style({
+          transform: 'rotate(0)'
+        })),
+        transition('collapse => expand', [
+          animate(400, style({
+            transform: 'rotate(0)'
+          }))
+        ]),
+        transition('expand => collapse', [
+          animate(400, style({
+            transform: 'rotate(180deg)'
+          }))
+        ])
+      ]),
+      trigger('memberCard', [
+          state('collapse', style({
+            transform: 'scale(1)'
+          })),
+          state( 'expand', style({
+            transform: 'scale(0)',
+            display: 'none'
+          })),
+          transition('collapse => expand', [
+            query('@initState', [
+                animateChild({ delay: '400' })
+            ]),
+            animate('700ms linear', style({
+              transform: 'scale(0)'
+            }))
+          ]),
+          transition('expand => collapse', [
+            query('@initState', [
+              animateChild({ delay: '400' })
+            ]),
+            animate('700ms linear', style({
+              transform: 'scale(1)'
+            }))
+          ])
+      ]),
+      trigger('membersCard', [
+          state('collapse', style({
+            transform: 'scale(0)',
+            display: 'none'
+          })),
+          state( 'expand', style({
+            transform: 'scale(1)'
+          })),
+          transition('collapse => expand', [
+            query('@returnState', [
+              animateChild()
+            ]),
+            animate('700ms linear', style({
+              transform: 'scale(1)'
+            }))
+          ]),
+          transition('expand => collapse', [
+            query('@returnState', [
+              animateChild()
+            ]),
+            animate('700ms linear', style({
+              transform: 'scale(0)'
+            }))
+          ])
+      ]),
+      trigger('alertCard', [
+          state('collapse', style({
+            transform: 'translate(0, 0)'
+          })),
+          state('expand', style({
+            transform: 'translate(0, 0)'
+          })),
+          transition('collapse => expand', [
+              animate('700ms linear', style({
+                transform: 'translate(0, 0)'
+              }))
+          ]),
+          transition('expand => collapse', [
+              animate('700ms linear', style({
+                transform: 'translate(0, 0)'
+              }))
+          ])
+      ])
+  ]
 })
 export class HomePage implements OnInit {
   pigCount = 1345;
@@ -17,6 +125,8 @@ export class HomePage implements OnInit {
   team: any[] = [];
   displayMember;
   displayAll = false;
+  state = 'collapse';
+  reverseState = 'collapse';
 
   alerts = [
     {
@@ -53,6 +163,8 @@ export class HomePage implements OnInit {
 
   toggleMembers() {
     this.displayAll = !this.displayAll;
+    this.state === 'collapse' ? this.state = 'expand' : this.state = 'collapse';
+    this.reverseState === 'collapse' ? this.reverseState = 'expand' : this.reverseState = 'collapse';
   }
 
   ngOnInit(): void {
